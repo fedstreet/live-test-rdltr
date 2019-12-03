e3add1e83536f5f44fc76668e3c5fd6b2a54df13
fedstreet <leontyev.ol2@gmail.com>
2019-12-03 13:03:23 +0300

    Add test config

---
+++ testrdltr/config/local_configurations/default_config.py
@@ -0,0 +1,3 @@
+API_BASE_URL = "http://localhost:5000/api"
+DEFAULT_USER_EMAIL = "a@a.a"
+DEFAULT_USER_PASSWORD = "userpass"
---
+++ testrdltr/config/testconfig.py
@@ -0,0 +1,14 @@
+from testrdltr.config.local_configurations.default_config import \
+    API_BASE_URL, DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD
+
+
+def get_base_api_url():
+    return API_BASE_URL
+
+
+def get_user_email():
+    return DEFAULT_USER_EMAIL
+
+
+def get_user_pasword():
+    return DEFAULT_USER_PASSWORD
